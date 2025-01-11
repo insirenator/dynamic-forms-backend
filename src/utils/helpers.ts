898aefb920574096a8a75837f8021535e3325a89
mohdshakeeb <arsshakeeb149@protonmail.com>
2025-01-11 13:53:33 +0530

    (feat/signup) use nanoid to generate signup token

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,5 +1,6 @@
 import bcrypt from "bcrypt";
-
+import { nanoid, customAlphabet } from "nanoid";
+// ------- HASHER ----------------
 export interface Hasher {
     hash: (text: string) => Promise<string>
     compare: (text: string, hash: string) => Promise<boolean>
@@ -11,5 +12,26 @@ export const hasher: Hasher = {
 
     async compare(text: string, hash: string) {
         return bcrypt.compare(text, hash);
+    }
+}
+
+// ------- TOKEN GENERATOR ---------
+export interface TokenGenerator {
+    generate: () => string
+    generateShort: () => string
+}
+
+export const tokenGenerator: TokenGenerator = {
+    // generates a lengthy id
+    generate() {
+        return nanoid(50);
+    },
+
+    generateShort() {
+        const smalls = "abcdefghijklmnopqrstuvwxyz";
+        const caps = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        const nums = "0123456789";
+
+        return customAlphabet(smalls + caps + nums, 8)();
     }
 }
