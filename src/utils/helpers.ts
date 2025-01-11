17c26097e55276159797ca831449c26916ab2194
mohdshakeeb <arsshakeeb149@protonmail.com>
2025-01-11 13:15:12 +0530

    (feat/signup) hash the password beforing storing in db

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,3 +1,15 @@
-export function sayHello(name: string) {
-    return "Hello, " + name;
+import bcrypt from "bcrypt";
+
+export interface Hasher {
+    hash: (text: string) => Promise<string>
+    compare: (text: string, hash: string) => Promise<boolean>
+}
+export const hasher: Hasher = {
+    async hash(text: string) {
+        return bcrypt.hash(text, 10);
+    },
+
+    async compare(text: string, hash: string) {
+        return bcrypt.compare(text, hash);
+    }
 }
